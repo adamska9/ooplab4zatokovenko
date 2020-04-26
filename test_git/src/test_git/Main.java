3d1398b2c66d8a96ea65d1cc33b9ea0a94afe733
DZ <DZ@faultier>
2020-04-26 18:56:13 +0300

    changed main.java

--- test_git/src/test_git/Main.java
+++ test_git/src/test_git/Main.java
@@ -2,6 +2,7 @@ package test_git;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("before change");
+		System.out.println("after change");
+		System.out.println("after change but make it 2");
 	}
 }
