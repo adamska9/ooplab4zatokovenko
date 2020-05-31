6a8a5f670bd36b3086d5940b57a0df99e8c06cae
adamska9 <54859102+adamska9@users.noreply.github.com>
2020-05-31 23:00:40 +0300

    Delete Main.java

--- test_git/src/test_git/Main.java
+++
@@ -1,8 +0,0 @@
-package test_git;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("after change");
-		System.out.println("after change but make it 2");
-	}
-}
